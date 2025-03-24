23a319814f2d29aaf5ff658ff13ccb8d180c6d89
AlexKh <khrypunalex@yahoo.com>
2025-03-24 15:25:18 +0100

    Initial setup for education project

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLICK_APP_NAME || 'Prostore';
+export const APP_DESCRIPTION = process.env.NEXT_PUBLICK_APP_DESCRIPTION || 'A modern ecomerce platform built with Next.js';
+export const SERVER_URL = process.env.NEXT_PUBLICK_SERVER_URL || 'http://localhost:3000';
